9dbe2e176f125fb7d94ecdefc1eb4b94c78e6852
RaIN <razor13372k@gmail.com>
2017-01-08 00:04:55 +0100

    Added Files

---
+++ AccountManager/AccountManager/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace AccountManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackgroundImageTiled = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(363, 224);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ADD ACCOUNT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "REMOVE ACCOUNT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(168, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "SET BANNED";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(15, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(168, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "SET 24H COOLDOWN";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(381, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(201, 106);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Location = new System.Drawing.Point(381, 124);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(201, 112);
+            this.panel2.TabIndex = 9;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(15, 14);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(168, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "SET READY";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 253);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "x1337zZHacker Account Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ AccountManager/AccountManager/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace AccountManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //TODO: UNFINISHED
+        private void CheckCooldown()
+        {
+            var Expired = false;
+
+            var BW = new BackgroundWorker();
+            BW.DoWork += delegate
+            {
+                while (!Expired)
+                {
+                    var myDateTime = DateTime.Parse(DateTime.Now.ToShortTimeString());
+                    var CurrentTime = Convert.ToInt64(myDateTime.ToString());
+
+                    //TODO: UNFINISHED
+                    var ConfigTime = 24234234234;
+
+                    if (CurrentTime >= ConfigTime)
+                        Expired = true;
+                }
+            };
+
+            BW.RunWorkerCompleted += delegate
+            {
+                //TODO: UNFINISHED
+            };
+
+            BW.RunWorkerAsync();
+        }
+
+        // Form Load Event
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+
+            //Add column header
+            listView1.Columns.Add("ACCOUNT NAME", 120);
+            listView1.Columns.Add("BAN STATUS", 100);
+            listView1.Columns.Add("READY STATUS", 139);
+
+            if (File.Exists("Accounts.xml"))
+            {
+                var doc = new XmlDocument();
+                doc.Load("Accounts.xml");
+
+                var AccountsContainer = doc.DocumentElement.SelectSingleNode("/Informations/Accounts");
+
+                var arr = new string[4];
+                ListViewItem itm;
+
+                foreach (XmlNode subNode in AccountsContainer)
+                {
+                    arr[0] = subNode.Attributes.GetNamedItem("VALUE").InnerText;
+                    arr[1] = subNode.ChildNodes[1].InnerText;
+                    arr[2] = subNode.ChildNodes[0].InnerText;
+
+                    itm = new ListViewItem(arr);
+                    listView1.Items.Add(itm);
+                }
+
+                foreach (var item in listView1.Items.Cast<ListViewItem>())
+                    if (item.SubItems[2].Text.Equals("READY") && item.SubItems[1].Text.Equals("CLEAN"))
+                    {
+                        item.BackColor = Color.Green;
+                    }
+                    else if (item.SubItems[2].Text.Equals("BANNED") || item.SubItems[1].Text.Equals("BANNED"))
+                    {
+                        item.SubItems[1].Text = "BANNED";
+                        item.SubItems[2].Text = "BANNED";
+                        item.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        item.BackColor = Color.DeepPink;
+                    }
+            }
+            else
+            {
+                File.WriteAllText("Accounts.xml", "<Informations>\r\n  <Accounts>\r\n  </Accounts>\r\n</Informations>");
+            }
+        }
+
+        // Add Account Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var AlreadyExists = false;
+
+            if (textBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Textbox is empty");
+                return;
+            }
+
+            foreach (var item in listView1.Items.Cast<ListViewItem>())
+                if (item.Text.Equals(textBox1.Text))
+                {
+                    MessageBox.Show("Already Exists");
+                    return;
+                }
+
+            var arr = new string[4];
+            ListViewItem itm;
+
+            arr[0] = textBox1.Text;
+
+            itm = new ListViewItem(arr);
+            listView1.Items.Add(itm);
+
+            if (File.Exists("Accounts.xml"))
+            {
+                var doc = new XmlDocument();
+                doc.Load("Accounts.xml");
+
+                var AccountsContainer = doc.DocumentElement.SelectSingleNode("/Informations/Accounts");
+
+                var subNode = AccountsContainer;
+
+                var NAME = doc.CreateElement("Name");
+                NAME.SetAttribute("VALUE", textBox1.Text);
+                var COOLDOWNSTATUS = doc.CreateElement("COOLDOWNSTATUS");
+                COOLDOWNSTATUS.InnerText = "READY";
+                var BANSTATUS = doc.CreateElement("BANSTATUS");
+                BANSTATUS.InnerText = "CLEAN";
+
+                NAME.AppendChild(COOLDOWNSTATUS);
+                NAME.AppendChild(BANSTATUS);
+                subNode.AppendChild(NAME);
+
+                doc.Save("Accounts.xml");
+            }
+        }
+
+        // Remove Acount Button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Select something first");
+                return;
+            }
+
+            if (File.Exists("Accounts.xml"))
+            {
+                var doc = new XmlDocument();
+                doc.Load("Accounts.xml");
+
+                var AccountsContainer = doc.DocumentElement.SelectSingleNode("/Informations/Accounts");
+
+                foreach (XmlNode subNode in AccountsContainer)
+                    if (subNode.Attributes.GetNamedItem("VALUE").InnerText.Equals(listView1.SelectedItems[0].Text))
+                    {
+                        subNode.ParentNode.RemoveChild(subNode);
+                        listView1.SelectedItems[0].Remove();
+                    }
+
+                doc.Save("Accounts.xml");
+            }
+        }
+
+        // Set READY Button
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Select something first");
+                return;
+            }
+
+            listView1.SelectedItems[0].SubItems[1].Text = "CLEAN";
+            listView1.SelectedItems[0].SubItems[2].Text = "READY";
+            listView1.SelectedItems[0].BackColor = Color.Green;
+
+            if (File.Exists("Accounts.xml"))
+            {
+                var doc = new XmlDocument();
+                doc.Load("Accounts.xml");
+
+                var AccountsContainer = doc.DocumentElement.SelectSingleNode("/Informations/Accounts");
+
+                foreach (XmlNode subNode in AccountsContainer)
+                {
+                    if (subNode.Attributes.GetNamedItem("VALUE").InnerText.Equals(listView1.SelectedItems[0].Text))
+                    {
+                        subNode.ChildNodes[0].InnerText = "READY";
+                        subNode.ChildNodes[1].InnerText = "CLEAN";
+                    }
+
+                    doc.Save("Accounts.xml");
+                }
+            }
+        }
+
+        // Set Banned
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Select something first");
+                return;
+            }
+
+            listView1.SelectedItems[0].SubItems[1].Text = "BANNED";
+            listView1.SelectedItems[0].SubItems[2].Text = "BANNED";
+            listView1.SelectedItems[0].BackColor = Color.Red;
+
+            if (File.Exists("Accounts.xml"))
+            {
+                var doc = new XmlDocument();
+                doc.Load("Accounts.xml");
+
+                var AccountsContainer = doc.DocumentElement.SelectSingleNode("/Informations/Accounts");
+
+                foreach (XmlNode subNode in AccountsContainer)
+                {
+                    if (subNode.Attributes.GetNamedItem("VALUE").InnerText.Equals(listView1.SelectedItems[0].Text))
+                    {
+                        subNode.ChildNodes[0].InnerText = "BANNED";
+                        subNode.ChildNodes[1].InnerText = "BANNED";
+                    }
+
+                    doc.Save("Accounts.xml");
+                }
+            }
+        }
+
+        // 24H Cooldown Button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Select something first");
+                return;
+            }
+
+            var myDateTime = DateTime.Parse(DateTime.Now.ToShortTimeString());
+            myDateTime = myDateTime.AddHours(24);
+
+            listView1.SelectedItems[0].SubItems[2].Text = myDateTime.ToString();
+            listView1.SelectedItems[0].SubItems[1].Text = "CLEAN";
+            listView1.SelectedItems[0].BackColor = Color.DeepPink;
+
+            if (File.Exists("Accounts.xml"))
+            {
+                var doc = new XmlDocument();
+                doc.Load("Accounts.xml");
+
+                var AccountsContainer = doc.DocumentElement.SelectSingleNode("/Informations/Accounts");
+
+                foreach (XmlNode subNode in AccountsContainer)
+                {
+                    if (subNode.Attributes.GetNamedItem("VALUE").InnerText.Equals(listView1.SelectedItems[0].Text))
+                    {
+                        subNode.ChildNodes[0].InnerText = myDateTime.ToString();
+                        subNode.ChildNodes[1].InnerText = "CLEAN";
+                    }
+
+                    doc.Save("Accounts.xml");
+                }
+            }
+        }
+    }
+}
